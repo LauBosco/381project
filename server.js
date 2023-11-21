d59576fcaa34c387dd4de5755693971af5c27e95
LauBosco <144005390+LauBosco@users.noreply.github.com>
2023-11-21 21:04:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.get("/api/item/itemID/:itemID", function(req,res) {
         let criteria = {};
         criteria["itemID"] = req.params.itemID;
         handle_Find(res, criteria, function(foundItems){
-            return res.status(200).json(foundItems);
+            return res.status(200).foundItems;
         });
     } else {
         res.status(500).json({"error": "missing item ID"});
